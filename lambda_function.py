c7a6b33e534314853fd9fb4e254c8f761ffebe0d
Francesco Vergona <vergona@amazon.com>
2024-02-20 10:46:16 +0100

    First commit - Lambda Python with langchain, psycopg2

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,7 @@
 import sys
+from langchain_community.llms import Bedrock
+
 def handler(event, context):
+    # Simple test of psycopg2
+    # conn = psycopg2.connect("dbname=test user=postgres password=postgres")
     return 'Hello from AWS Lambda using Python' + sys.version + '!'
